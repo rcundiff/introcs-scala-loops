5a03b64ceadf94d6b2854ae57c164dafc536f997
rcundiff1 <rcundiff1@luc.edu>
2015-03-23 15:44:02 +0000

    Finished project

--- loops.scala
+++ loops.scala
@@ -5,14 +5,18 @@ object loops {
   // Return a string containing n copies of s, end to end.
   // For example StringOfReps("Ok", 9) returns: "OkOkOkOkOkOkOkOkOk"
   def stringOfReps(s: String, n: Int): String = {
-    "unknown"
+     s * n
   }
 
   // Return n! (n factorial: 1*2*3 *...* n if n >=1; 0! is 1.).  
   //For example Factorial(4) returns 1*2*3*4 = 24.
-  def factorial(n: Int): Int = {
-    0
-  }
+  def factorial(n : Int): Int = {
+        if (n <= 1) 
+            return 1
+         else
+           n * factorial(n-1)              
+    }
+    
 
 
    // Return a string containing a filled rectange, where the filling is 
@@ -24,7 +28,23 @@ object loops {
    //    eiiie
    //    eeeee
    def printRectangle(columns:Int, rows:Int, inChar:String, edgeChar:String): String = {
-     "unknown"
+       
+    var upperouter = edgeChar * (columns + rows)
+
+    var insidecolumns = (columns + rows) - 2
+
+    var inside = edgeChar + (inChar * insidecolumns) + edgeChar
+
+
+    var data = ""
+    data += upperouter
+    for( a <- 1 to rows){
+      data += "\n" + inside
+    }
+
+    data += "\n" + upperouter
+
+    return data
    }
 
 }
